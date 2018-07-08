c758aea99cfad64c11619436ff2905b1531b1436
Chanple <chanple@outlook.com>
2018-07-08 19:26:30 +0800

    Store

---
+++ Source/SmartTaskbar/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace SmartTaskbar
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            //Use a mutex to ensure single instance
+            using (Mutex mutex = new Mutex(true, "{32abdf83-53b8-48c4-a689-c65fdd3fcd5a}", out bool createNew))
+            {
+                if (!createNew)
+                    return;
+                Application.EnableVisualStyles();
+                Application.SetCompatibleTextRenderingDefault(false);
+                //Start a tray instead of a WinForm to reduce memory usage
+                new SystemTray();
+                Application.Run();
+            }
+        }
+    }
+}
---
+++ Source/SmartTaskbar/Switcher/TaskbarSwitcher.cs
@@ -0,0 +1,67 @@
+﻿using System.Diagnostics;
+using System.IO;
+using static SmartTaskbar.SafeNativeMethods;
+
+namespace SmartTaskbar
+{
+    class TaskbarSwitcher
+    {
+        private Process process = new Process();
+        private APPBARDATA msgData = APPBARDATA.New();
+        private ChildProcessTracker child = new ChildProcessTracker();
+        private bool isStop = true, animation;
+
+        public TaskbarSwitcher() => process.StartInfo.FileName = Path.Combine(System.Windows.Forms.Application.StartupPath, "..", "TaskbarSwitcher", "TaskbarSwitcher");
+
+        public void Hide()
+        {
+            Stop();
+            msgData.lParam = AutoHide;
+            SHAppBarMessage(SetState, ref msgData);
+        }
+
+        public void Show()
+        {
+            Stop();
+            msgData.lParam = AlwaysOnTop;
+            SHAppBarMessage(SetState, ref msgData);
+        }
+
+        public bool IsHide() => SHAppBarMessage(GetState, ref msgData) == AutoHide ? true : false;
+
+        public void Start()
+        {
+            isStop = false;
+            process.Start();
+            child.AddProcess(process);
+        }
+
+        public void Resume()
+        {
+            if (process.HasExited)
+                Start();
+        }
+
+        public void Stop()
+        {
+            if (isStop)
+                return;
+            isStop = true;
+            process.Kill();
+            process.WaitForExit();
+        }
+
+        public bool IsAnimationEnable()
+        {
+            GetSystemParameters(SPI_GETMENUANIMATION, 0, out animation, 0);
+            return animation;
+        }
+
+        public bool AnimationSwitcher()
+        {
+            animation = !animation;
+            SetSystemParameters(SPI_SETMENUANIMATION, 0, animation, 0x01 | 0x02);
+            return animation;
+        }
+    }
+}
